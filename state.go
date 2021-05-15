d35d515ac80a43dee929496f7c2a51bb616e82ad
Viswajith S Valsan <65163117+viswa@users.noreply.github.com>
2021-05-15 19:26:35 +0530

    Make present state variable exported

--- state.go
+++ state.go
@@ -6,18 +6,18 @@ import (
 
 // State represents the present and future state of Universe
 type State struct {
-	present, future *Universe
+	Present, future *Universe
 	width, height   int
 }
 
 // NewState returns a new State with random initial values
 func NewState(w, h int) *State {
-	present := NewUniverse(w, h)
+	Present := NewUniverse(w, h)
 	for i := 0; i < (w * h / 4); i++ {
-		present.Set(rand.Intn(w), rand.Intn(h), true)
+		Present.Set(rand.Intn(w), rand.Intn(h), true)
 	}
 	return &State{
-		present: present, future: NewUniverse(w, h),
+		Present: Present, future: NewUniverse(w, h),
 		width: w, height: h,
 	}
 }
@@ -27,9 +27,9 @@ func (s *State) Step() {
 	// Update future from the current state
 	for y := 0; y < s.height; y++ {
 		for x := 0; x < s.width; x++ {
-			s.future.Set(x, y, s.present.Next(x, y))
+			s.future.Set(x, y, s.Present.Next(x, y))
 		}
 	}
 	// Swap future with present
-	s.present, s.future = s.future, s.present
+	s.Present, s.future = s.future, s.Present
 }
